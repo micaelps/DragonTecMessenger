ddced2bd96f19f87649cc67d133fb23bf66f68f3
Micael <3.1416micael@gmail.com>
2020-06-28 12:32:27 -0300

    Login screen

---
+++ app/src/main/java/com/micaelps/dragontecmessenger/LoginActivity.java
@@ -0,0 +1,52 @@
+package com.micaelps.dragontecmessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mEditEmail;
+    private EditText mEditPassword;
+    private Button mBtnEnter;
+    private TextView mTxtAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_login);
+
+
+
+        mEditEmail = findViewById(R.id.edit_email);
+        mEditPassword = findViewById(R.id.edit_password);
+        mBtnEnter = findViewById(R.id.btn_enter);
+        mTxtAccount = findViewById(R.id.txt_account);
+
+        mBtnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEditEmail.getText().toString();
+                String password = mEditPassword.getText().toString();
+
+                Log.i("Teste",email);
+                Log.i("Teste",password);
+
+            }
+        });
+
+        mTxtAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/micaelps/dragontecmessenger/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.micaelps.dragontecmessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_register);
+    }
+}
