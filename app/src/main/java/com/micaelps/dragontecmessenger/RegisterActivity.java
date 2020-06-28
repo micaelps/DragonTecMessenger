a536292aba463ccbab355d52a482f1690fd07667
Micael <3.1416micael@gmail.com>
2020-06-28 19:49:28 -0300

    register screen && create user

--- app/src/main/java/com/micaelps/dragontecmessenger/RegisterActivity.java
+++ app/src/main/java/com/micaelps/dragontecmessenger/RegisterActivity.java
@@ -1,14 +1,71 @@
 package com.micaelps.dragontecmessenger;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText mEditUsername;
+    private EditText mEditEmail;
+    private EditText mEditPassword;
+    private Button mBtnEnter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_register);
+
+        mEditUsername = findViewById(R.id.edit_username);
+        mEditEmail = findViewById(R.id.edit_email);
+        mEditPassword = findViewById(R.id.edit_password);
+        mBtnEnter = findViewById(R.id.btn_enter);
+
+        mBtnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+
+            }
+        });
     }
+
+    private void createUser(){
+        String email = mEditEmail.getText().toString();
+        String senha = mEditPassword.getText().toString();
+        if(email==null || email.isEmpty() || senha.isEmpty()){
+            Toast.makeText(this,"Senha e email devem ser preenchidos",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.i("Sucesso",task.getResult().getUser().getUid());
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.i("Falhou",e.getMessage());
+            }
+        });
+    }
+
+
 }
